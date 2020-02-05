f67fd189e1fe822fc2fcb23214c30374de8c0132
Peter Schoener <peter.c.schoener@gmail.com>
2020-02-04 21:12:36 -0800

    convert the conll to text for embedding training

---
+++ src/as-text.rs
@@ -0,0 +1,30 @@
+use std::io;
+
+fn main() {
+    loop {
+        let mut line: String = String::new();
+        if io::stdin()
+            .read_line(&mut line)
+            .expect("Could not read line")
+            == 0
+        {
+            break;
+        }
+
+        if line.trim().is_empty() {
+            continue;
+        }
+        
+        if *(line.chars().peekable().peek().expect("Empty input line")) == '#' {
+            continue;
+        }
+
+        print!(
+            "{} ",
+            match line.split_whitespace().nth(1) {
+                Some(s) => s,
+                _ => "",
+            }
+        );
+    }
+}
