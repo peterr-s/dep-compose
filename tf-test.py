a922f081c4d63538057483169cd6512386f8bb77
Peter Schoener <peter.c.schoener@gmail.com>
2020-04-11 19:49:03 -0700

    made shape condusive to cpu training
    however it is still looking between samples; clearly I am misinterpreting a dimension

--- tf-test.py
+++ tf-test.py
@@ -5,23 +5,24 @@ import numpy as np
 
 batch_size = 10
 
-embed_dim = 100
-x1 = np.random.rand(5000, 1, embed_dim)
-x2 = np.random.rand(5000, 1, embed_dim)
+embed_dim = 3
+x1 = np.random.rand(50000, 1, embed_dim)
+x2 = np.random.rand(50000, 1, embed_dim)
 y = x1 + x2
 
 a = tf.keras.Input(shape = (1, embed_dim))
 b = tf.keras.Input(shape = (1, embed_dim))
 
 conc = tf.keras.layers.concatenate([a, b], axis = 1)
-conv = tf.keras.layers.Conv1D(filters = 1, kernel_size = embed_dim, data_format = "channels_first")(conc)
-output = tf.keras.layers.Dense(100)(conv)
+perm = tf.keras.layers.Permute((2, 1))(conc)
+conv = tf.keras.layers.Conv1D(filters = 50, kernel_size = embed_dim)(perm)
+output = tf.keras.layers.Dense(embed_dim)(conv)
 
 model = tf.keras.Model(inputs = [a, b], outputs = (output))
 
 model.compile(optimizer = tf.keras.optimizers.SGD(), loss = tf.keras.losses.MeanSquaredError())
 
-model.fit([x1, x2], y, batch_size = batch_size, epochs = 20)
+model.fit([x1, x2], y, batch_size = batch_size, epochs = 3)
 
 y_pred = model.predict((np.array([[[0.1, 0.2, 0.3]], [[0.5, 0.5, 0.6]]]), np.array([[[0.2, 0.3, 0.4]], [[0.01, 0.2, 0]]])))
 print(y_pred)
