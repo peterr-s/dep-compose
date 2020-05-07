7ae2e538721d9312baaa740636ba08d9c7564a7c
Peter Schoener <peter.c.schoener@gmail.com>
2020-05-07 15:17:14 -0700

    implemented graph building

--- model.py
+++ model.py
@@ -17,11 +17,6 @@ if len(sys.argv) < 4 :
 # load config from file
 config = DefaultConfig()
 
-# load embedding model
-embeddings = KeyedVectors.load_word2vec_format(sys.argv[3], binary = True)
-word_dim = embeddings.vector_size
-null_word = np.zeros(word_dim)
-
 # load parses
 parse_train = pyconll.load_from_file(sys.argv[1])
 parse_test = pyconll.load_from_file(sys.argv[2])
@@ -38,17 +33,40 @@ for parse in parse_test :
     for word in parse :
         dep_types.add(word.deprel)
 
+# load embedding model
+embeddings = KeyedVectors.load_word2vec_format(sys.argv[3], binary = True)
+word_dim = embeddings.vector_size
+null_word = np.zeros(word_dim)
+
 # define a layer for each possible dependency
 dep_layers = dict()
 for dep_type in dep_types :
-    dep_layers[dep_type] = model.add_parameters((config.sent_dim, 2 * word_dim))
+    dep_layers[dep_type] = model.add_parameters((config.sent_dim, 2 * config.sent_dim))
+
+# define trainable projection layer from word dim to phrase dim
+# this simplifies concatenation and allows us to treat the recursive base case as a phrase of its own
+word_to_phrase_projection = model.add_parameters((config.sent_dim, word_dim))
 
 # define graph building operation
 def generate_graph(parse) :
+    parse_graph = parse.to_tree()
+    return graph_gen_helper(parse_graph)
+
+def graph_gen_helper(node) :
+    node_value = word_to_phrase_projection * embeddings[node.data.form]
+
+    for child in node :
+        child_subtree = graph_gen_helper(child)
+
+        # concatenate the node so far with the subtree, select layer according to dep reln
+        node_value = dep_layers[child.data.deprel] * dynet.concatenate([node_value, child_subtree])
+
+    return node_value
 
 # run training
-for parse in parse_train :
-    loss = generate_graph(parse)
+for parse, y_pred in zip(parse_train, y_preds) :
+    y_pred = generate_graph(parse)
+    loss = dynet.l1_distance(dynet.l2_norm(y_pred), dynet.l2_norm(y))
 
 # run eval
 
