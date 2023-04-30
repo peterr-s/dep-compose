24dc3b10754b1d18b5332c02e32b4a36a7bbfde3
Peter Schoener <peter.c.schoener@gmail.com>
2023-04-29 18:13:53 -0700

    fix type and argument issues
    Also added quick and dirty sanity check script

---
+++ dry-run.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import torch
+from transformers import AutoTokenizer
+
+from model import Composer, DependencyEncoding
+
+t = AutoTokenizer.from_pretrained("distilgpt2")
+tok = t("this is a test", return_tensors = "pt")
+
+c = Composer(500, 6000, 500, 20, 500, 3, dtype = torch.float)
+d = DependencyEncoding(types = torch.LongTensor([0, 1, 2, 3]), heads = torch.LongTensor([1, 1, 1, 1]))
+
+result = c(tok.input_ids[0], d)
+print(result)
--- model.py
+++ model.py
@@ -1,52 +1,71 @@
-import torch
-import transformers
+from dataclasses import dataclass
 
+import torch
+
+@dataclass
 class DependencyEncoding :
-    def __init__(self, types, heads) :
-        self.types = types
-        self.heads = heads
+    types: torch.LongTensor
+    heads: torch.LongTensor
 
 class CompositionBlock(torch.nn.Module) :
-    def __init__(token_embedding_dim,
-            dep_embedding_dim,
-            dep_transformed_dim) :
+    def __init__(self,
+            token_embedding_dim: int,
+            dep_embedding_dim: int,
+            dep_transformed_dim: int,
+            dtype: torch.dtype = torch.float16) :
         super().__init__()
         
         self.dep_transform = torch.nn.Bilinear(token_embedding_dim,
                 dep_embedding_dim,
-                dep_transformed_dim)
+                dep_transformed_dim,
+                dtype = dtype)
+        self.dep_activation = torch.nn.Tanh()
         self.composition_transform = torch.nn.Bilinear(token_embedding_dim,
                 dep_transformed_dim,
-                token_embedding_dim)
+                token_embedding_dim,
+                dtype = dtype)
+        self.composition_activation = torch.nn.Tanh()
 
-    def forward(self, token_embeddings, dep_embeddings, dep_heads) :
+    def forward(self,
+            token_embeddings: torch.HalfTensor,
+            dep_embeddings: torch.HalfTensor,
+            dep_heads: torch.LongTensor) :
         token_dep_embeddings = self.dep_transform(token_embeddings, dep_embeddings)
-        token_dep_embeddings = torch.nn.Tanh(token_embeddings)
+        token_dep_embeddings = self.dep_activation(token_embeddings)
 
         token_embeddings = self.composition_transform(token_embeddings,
                 token_dep_embeddings)
-        token_embeddings = torch.nn.Tanh(token_embeddings)
+        token_embeddings = self.composition_activation(token_embeddings)
 
         return token_embeddings
 
-class Composer(transformers.PreTrainedModel) :
-    def __init__(token_embedding_dim,
-            token_embedding_ct,
-            dep_embedding_dim,
-            dep_embedding_ct,
-            depth) :
+class Composer(torch.nn.Module) :
+    def __init__(self,
+            token_embedding_dim: int,
+            token_embedding_ct: int,
+            dep_embedding_dim: int,
+            dep_embedding_ct: int,
+            dep_transformed_dim: int,
+            depth: int,
+            dtype: torch.dtype = torch.float16) :
         super().__init__()
         self.depth = depth
 
         self.token_embedding_layer = torch.nn.Embedding(token_embedding_ct,
-                token_embedding_dim)
+                token_embedding_dim,
+                dtype = dtype)
         self.dep_embedding_layer = torch.nn.Embedding(dep_embedding_ct,
-                dep_embedding_dim)
+                dep_embedding_dim,
+                dtype = dtype)
 
         self.composition_block = CompositionBlock(token_embedding_dim,
-                dep_embedding_dim)
+                dep_embedding_dim,
+                dep_transformed_dim,
+                dtype = dtype)
 
-    def forward(self, tokens, deps) :
+    def forward(self,
+            tokens: torch.LongTensor,
+            deps: DependencyEncoding) :
         token_embeddings = self.token_embedding_layer(tokens)
         dep_embeddings = self.dep_embedding_layer(deps.types)
 
