fix typing and precision issues
When using FP16, some layers created type incompatibilities. Fixed these
and cleaned up other typing to prevent future confusion.

Also, when using FP16, underflow in the optimizer can cause gradient
issues. See [PyTorch #26218](https://github.com/pytorch/pytorch/issues/26218).
Issue is still present with `eps = 1e-6`, so using `1e-4` for now.

composer/model.py
@@ -5,20 +5,21 @@ import torch
 import numpy as np
 
 def generate_mask(dep_heads: torch.LongTensor,
-        embedding_dim: int) :
+        embedding_dim: int) -> torch.BoolTensor :
     # populate rank-3 mask of tokens to whether each other token is their child
     # [batch_sz, seq_len, seq_len]
     dep_mask = np.zeros((*dep_heads.shape, dep_heads.shape[-1]), dtype = bool)
     for i, sample in enumerate(dep_heads) :
         for j, head in enumerate(sample) :
-            dep_mask[i, head, j] = True
+            if head < dep_heads.shape[-1] :
+                dep_mask[i, head, j] = True
 
     # copy across embedding axis to allow multiplication
     dep_mask = np.tile(
             np.expand_dims(dep_mask, len(dep_mask.shape)),
             embedding_dim)
 
-    return torch.Tensor(dep_mask)
+    return torch.BoolTensor(dep_mask)
 
 @dataclass
 class DependencyEncoding :
@@ -34,25 +35,26 @@ class CompositionBlock(torch.nn.Module) :
             dtype: torch.dtype = torch.float16) :
         super().__init__()
         self.seq_len = seq_len
+        self.dtype = dtype
         
         self.dep_transform = torch.nn.Bilinear(token_embedding_dim,
                 dep_embedding_dim,
                 dep_transformed_dim,
-                dtype = dtype)
+                dtype = self.dtype)
         self.dep_activation = torch.nn.Tanh()
 
         self.composition_transform = torch.nn.Bilinear(token_embedding_dim,
                 dep_transformed_dim,
                 token_embedding_dim,
-                dtype = dtype)
+                dtype = self.dtype)
         self.composition_activation = torch.nn.Tanh()
 
-        self.reduction_transform = torch.nn.Linear(self.seq_len, 1)
-        self.reduction_activation = torch.nn.Tanh()
+        self.reduction_transform = torch.nn.Linear(self.seq_len, 1, dtype = self.dtype)
+        self.reduction_activation = torch.nn.Softmax(dim = 1)
 
     def forward(self,
-            token_embeddings: torch.HalfTensor,
-            dep_embeddings: torch.HalfTensor,
+            token_embeddings: torch.Tensor,
+            dep_embeddings: torch.Tensor,
             dep_heads: torch.LongTensor) :
 
         # assert that we have an appropriate input shape (seq_len is respected)
@@ -60,9 +62,14 @@ class CompositionBlock(torch.nn.Module) :
         assert(dep_embeddings.shape[1] == self.seq_len)
         assert(dep_heads.shape[1] == self.seq_len)
 
+        # cast embeddings to correct dtype
+        if token_embeddings.dtype != self.dtype :
+            token_embeddings = token_embeddings.to(self.dtype)
+        if dep_embeddings.dtype != self.dtype :
+            dep_embeddings = dep_embeddings.to(self.dtype)
+
         # transform each embedding according to its dependency type relative to its head
         token_dep_embeddings = self.dep_transform(token_embeddings, dep_embeddings)
-        token_dep_embeddings = self.dep_activation(token_embeddings)
 
         # broadcast dependency-transformed embedding
         # [batch_sz, seq_len, embedding_dim] -> [batch_sz, seq_len, seq_len, embedding_dim]
@@ -73,7 +80,9 @@ class CompositionBlock(torch.nn.Module) :
         
         # create dependency mask so that each token is only going to be influenced by its children
         token_dep_mask = generate_mask(dep_heads, token_embeddings.shape[-1]).to(self.device())
-        token_dep_embeddings *= token_dep_mask
+        token_dep_embeddings = token_dep_embeddings.where(token_dep_mask,
+                torch.zeros(token_dep_embeddings.shape, device = self.device(), dtype = self.dtype))
+        token_dep_embeddings = self.dep_activation(token_dep_embeddings)
 
         # broadcast unchanged token embeddings for shape compatibility
         token_embeddings = token_embeddings.expand(self.seq_len,
@@ -147,7 +156,7 @@ class Composer(torch.nn.Module) :
         return next(self.parameters()).device
 
     def truncate_input(self, tensor: torch.LongTensor) -> torch.LongTensor :
-        return tensor[:self.composition_block.seq_len]
+        return tensor[:self.composition_block.seq_len].clone()
 
     def truncate_inputs(self,
             tokens: Optional[torch.LongTensor] = None,

composer/train.py
@@ -27,20 +27,20 @@ def main(corpus_path: str,
     log.info("training embedding model")
     corpus = CONLLCorpus(corpus_path)
     word_embeddings = Word2Vec(list(corpus.get_texts()),
-            vector_size = token_embedding_dim)
+            vector_size = token_embedding_dim,
+            min_count = 3)
 
     log.info("setting up composer")
     composer = Composer(token_embedding_dim,
-            15000,
-            2000,
+            10,
+            6000,
             dep_type_ct,
             token_embedding_dim,
             seq_len,
-            3,
-            dtype = torch.float)
+            2)
     log.debug(f"set up {composer=}")
     composer.token_embedding_layer = torch.nn.Embedding.from_pretrained(
-            torch.Tensor(word_embeddings.wv.vectors))
+            torch.HalfTensor(word_embeddings.wv.vectors))
     log.debug(f"replaced embedding layer; {composer=}")
     log.info(f"model has {sum(p.numel() for p in composer.parameters())} params")
 
@@ -50,7 +50,7 @@ def main(corpus_path: str,
     log.info(f"moved model to {device}")
 
     loss_fn = torch.nn.CosineEmbeddingLoss()
-    optimizer = torch.optim.Adam(composer.parameters(), lr = 0.001)
+    optimizer = torch.optim.Adam(composer.parameters(), lr = 0.001, eps = 1e-05)
 
     log.info("loading composer training data")
     dataset = ComposerCONLLIterableDataset(get_conll_file_paths(train_path),
@@ -65,7 +65,7 @@ def main(corpus_path: str,
             word_embeddings.wv.has_index_for,
             composer.pad_inputs)
     dataloader_val = torch.utils.data.DataLoader(dataset_val,
-            batch_size = val_batch_sz)
+            batch_size = batch_sz)
 
     log.info(f"starting training run, {epochs=}")
     for epoch_no in range(epochs) :