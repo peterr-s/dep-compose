fcb1ab4792138e9a50c344beed5292c3361330d1
Peter Schoener <peter.c.schoener@gmail.com>
2023-05-21 04:30:54 -0700

    implement dataset for dynamic composer variant

--- composer/data.py
+++ composer/data.py
@@ -5,7 +5,7 @@ import torch
 
 from composer.utils import find_nps_in_tree
 
-class ComposerCONLLIterableDataset(torch.utils.data.IterableDataset) :
+class CONLLIterableDatasetBase(torch.utils.data.IterableDataset) :
     def __init__(self,
             paths,
             token_id_getter,
@@ -29,22 +29,69 @@ class ComposerCONLLIterableDataset(torch.utils.data.IterableDataset) :
                             self._id_to_dep.append(dep)
 
                     for np in find_nps_in_tree(sentence.to_tree()) :
-                        token_list = np.to_list()
-
-                        form_list = [token.get("form") for token in token_list]
-                        target_token = "_".join(form_list)
-                        if not self.token_presence_checker(target_token) :
+                        if (sample := self.create_sample(np)) is not None :
+                            yield sample
+                        else :
                             continue
 
-                        input_ids = torch.LongTensor([self.token_id_getter(form)
-                                for form in form_list])
-                        output_id = torch.LongTensor([self.token_id_getter(target_token)])
+    def create_sample(self, *args, **kwargs) :
+        raise NotImplementedError()
 
-                        dep_ids = torch.LongTensor([self._dep_to_id[token.get("deprel")]
-                                for token in token_list])
+class ComposerCONLLIterableDataset(CONLLIterableDatasetBase) :
+    def create_sample(self, np) :
+        token_list = np.to_list()
 
-                        id_to_idx = {token.get("id") : i for i, token in enumerate(token_list)}
-                        head_idcs = torch.LongTensor([id_to_idx.get(token.get("head"), 0)
-                                for token in token_list])
+        form_list = [token.get("form") for token in token_list]
+        target_token = "_".join(form_list)
+        if not self.token_presence_checker(target_token) :
+            return None
 
-                        yield *self.pad_fn(input_ids, dep_ids, head_idcs), output_id
+        input_ids = torch.LongTensor([self.token_id_getter(form)
+                for form in form_list])
+        output_id = torch.LongTensor([self.token_id_getter(target_token)])
+
+        dep_ids = torch.LongTensor([self._dep_to_id[token.get("deprel")]
+                for token in token_list])
+
+        id_to_idx = {token.get("id") : i for i, token in enumerate(token_list)}
+        head_idcs = torch.LongTensor([id_to_idx.get(token.get("head"), 0)
+                for token in token_list])
+
+        return *self.pad_fn(input_ids, dep_ids, head_idcs), output_id
+
+class DynamicComposerCONLLIterableDataset(CONLLIterableDatasetBase) :
+    def _create_layer(self, phrase, nodes) :
+        layer = [None] * len(phrase)
+        for node in nodes :
+            layer[phrase.index(node)] = node
+
+    def create_sample(self, np) :
+        layers = [[np]]
+        while layer := self._create_layer(np,
+                [node.children for node in layers[-1]]) :
+            layers.append(layer)
+
+        token_list = np.to_list()
+
+        form_list = [token.get("form") for token in token_list]
+        target_token = "_".join(form_list)
+        if not self.token_presence_checker(target_token) :
+            return None
+
+        input_ids = torch.LongTensor([self.token_id_getter(form)
+                for form in form_list])
+        output_id = torch.LongTensor([self.token_id_getter(target_token)])
+
+        # TODO use a reserved dep type id when position is not applicable to layer
+        dep_ids = [torch.LongTensor([0 if node is None else self._dep_to_id[node.token.get("deprel")]
+                for node in layer]) for layer in layers]
+
+        id_to_idx = {token.get("id") : i for i, token in enumerate(token_list)}
+        head_idcs = [torch.LongTensor([0 if node is None else id_to_idx.get(node.token.get("head"), 0)
+                for node in layer]) for layer in layers]
+
+        return (self.pad_fn(input_ids = input_ids)[0],
+                [self.pad_fn(dep_ids = layer_dep_ids)[1] for layer_dep_ids in dep_ids],
+                [self.pad_fn(head_idcs = layer_head_idcs)[2] for layer_head_idcs in head_idcs],
+                output_id)
+
